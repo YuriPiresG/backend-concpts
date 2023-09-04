f8dfba2bfe3ffaa75401bc67f9272df8a0a9ee4b
Yuri Pires <yuripiresbr@gmail.com>
2023-09-04 10:21:45 -0300

    feat: created product entity

---
+++ src/product/entities/product.entity.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  descricao: string;
+
+  @Column()
+  unidade: string;
+
+  @Column()
+  volume: number;
+
+  @Column()
+  marca: string;
+
+  @Column()
+  peso: string;
+}
